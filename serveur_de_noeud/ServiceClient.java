46e80b7577d9268a9d0e0e8bdf88de13a94307fe
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-21 14:42:57 +0100

    Ajout du service Client

--- serveur_de_noeud/ServiceClient.java
+++ serveur_de_noeud/ServiceClient.java
@@ -2,5 +2,11 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ServiceClient extends Remote{
-  
+
+  /**
+   * Methode qui permet de compter le nombre de mots d'un texte
+   * @param texte chaine de caracteres a compter
+   * @return Nombre de mots present dans le texte donnee en parametre
+   */
+  public int compterMots(String texte) throws RemoteException;
 }

27ae4c5788938e81d0ecf35fd9bd5b28cdbf74db
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-21 15:48:07 +0100

    ajout client

--- client/Client.java
+++ client/Client.java
@@ -1,5 +1,12 @@
-
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class Client {
-  
+
+  public static void main(String[] args) {
+
+    // Adresse a changer en fonction de l'ip du serveur de noeud
+    Registry reg = LocateRegistry.getRegistry("localhost",1099);
+  }
 }

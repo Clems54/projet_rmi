66b4fe8971a4a219822d8cc31957990872458f64
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-21 15:17:18 +0100

    modif javadoc des erveurs de noeud

--- serveur_de_noeud/ServiceNoeud.java
+++ serveur_de_noeud/ServiceNoeud.java
@@ -5,6 +5,7 @@ public interface ServiceNoeud extends Remote{
 
   /**
    * Methode qui permet au noeud de calcul d'aider au comptage du texte
+   * @param noeud_de_calcul le noeud de calcul qui souhaite aider au comptage du texte
    */
   public void jeSouhaiteCalculer(ServiceNoeud noeud_de_calcul) throws RemoteException;
 }

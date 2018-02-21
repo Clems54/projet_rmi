b3b83694295233bcc98a8c40863cc71fa242e7c7
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-21 15:25:49 +0100

    Ajout gestion de client serveur de noeud

---
+++ serveur_de_noeud/GestionClient.java
@@ -0,0 +1,8 @@
+
+
+public class GestionClient implements ServiceClient{
+
+  public int compterMots(File texte) throws RemoteException{
+    
+  }
+}
--- serveur_de_noeud/ServiceClient.java
+++ serveur_de_noeud/ServiceClient.java
@@ -5,8 +5,8 @@ public interface ServiceClient extends Remote{
 
   /**
    * Methode qui permet de compter le nombre de mots d'un texte
-   * @param texte chaine de caracteres a compter
+   * @param texte fichier avec texte a compter
    * @return nombre de mots present dans le texte donnee en parametre
    */
-  public int compterMots(String texte) throws RemoteException;
+  public int compterMots(File texte) throws RemoteException;
 }

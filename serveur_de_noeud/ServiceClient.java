3aa103065837c0f7918cc61b912100f410146086
Clems <clement.mercier3@etu.univ-lorraine.fr>
2018-02-21 14:52:25 +0100

    Modif javadoc ServiceClient

--- serveur_de_noeud/ServiceClient.java
+++ serveur_de_noeud/ServiceClient.java
@@ -6,7 +6,7 @@ public interface ServiceClient extends Remote{
   /**
    * Methode qui permet de compter le nombre de mots d'un texte
    * @param texte chaine de caracteres a compter
-   * @return Nombre de mots present dans le texte donnee en parametre
+   * @return nombre de mots present dans le texte donnee en parametre
    */
   public int compterMots(String texte) throws RemoteException;
 }
